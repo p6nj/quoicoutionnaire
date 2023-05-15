4ef4342746de8d0d9c0c8a5f93d37ec6c87b9956
p6nj <breee@duck.com>
2023-05-15 18:05:11 +0200

    Add sql script, sort letters, add static

---
+++ static/submit.html
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="style.css">
+    <title>Quoicoutionnaire</title>
+</head>
+
+<body>
+
+    <h1>Quoicoutionnaire</h1>
+    <sub>LA référence</sub>
+
+    <h2>Soumettre votre quoicoumot</h2>
+    Vous pouvez soumettre votre propre quoicoumot à la base :
+    <form action="submit.result.php" method="post">
+        <input type="text" name="mot"></input>
+        <input type="submit" name="ok" value="lets go!!">
+    </form>
+
+</body>
+
+</html>
--- submit.result.php
+++ submit.result.php
@@ -5,45 +5,42 @@
     <link rel="stylesheet" href="style.css">
     <title>Quoicoutionnaire</title>
 </head>
+<?php
 
-<error>
-    <?php
+$success = false;
 
-    $success = false;
+$servername = "localhost";
+$username = "php";
+$password = "K&a=K!pKv~5kf8qa";
 
-    $servername = "localhost";
-    $username = "php";
-    $password = "K&a=K!pKv~5kf8qa";
+$status = "";
 
-    $status = "";
-
-    function main()
-    {
-        global $success, $servername, $username, $password, $status;
-        // Create connection
-        $conn = new mysqli($servername, $username, $password);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Erreur de connexion: " . $conn->connect_error);
-        }
-        $conn->query("use quoicou");
-        if ($_POST["mot"] == '') {
-            $status = "Le mot ne peut pas être vide !";
-            return;
-        }
-        if (strtolower(substr($_POST["mot"], 0, 7)) != 'quoicou')
-            die("Le mot doit commencer par 'quoicou'.");
-        $sql = "insert into mots(label) values ('" . $_POST["mot"] . "')";
-        try {
-            if ($conn->query($sql))
-                $success = true;
-        } catch (mysqli_sql_exception $ex) {
-            $status = $ex->getCode() == 1062 ? 'Ce mot existe déjà dans la base !' : $conn->error;
-        }
+function main()
+{
+    global $success, $servername, $username, $password, $status;
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Erreur de connexion: " . $conn->connect_error);
     }
-    main();
-    ?>
-</error>
+    $conn->query("use quoicou");
+    if ($_POST["mot"] == '') {
+        $status = "Le mot ne peut pas être vide !";
+        return;
+    }
+    if (strtolower(substr($_POST["mot"], 0, 7)) != 'quoicou')
+        die("Le mot doit commencer par 'quoicou'.");
+    $sql = "insert into mots(label) values ('" . $_POST["mot"] . "')";
+    try {
+        if ($conn->query($sql))
+            $success = true;
+    } catch (mysqli_sql_exception $ex) {
+        $status = $ex->getCode() == 1062 ? 'Ce mot existe déjà dans la base !' : $conn->error;
+    }
+}
+main();
+?>
 
 <body>
 
